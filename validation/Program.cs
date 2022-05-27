5c9371481d145813b7585efa3ad47a1e0152f801
puja <pujarana1796@gmail.com>
2022-05-27 18:17:37 +0530

    lambda expression for validation

---
+++ validation/Program.cs
@@ -0,0 +1,49 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Text.RegularExpressions;
+
+namespace validation
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            ValidationusingLambda v1 = new();
+            Console.WriteLine("enter firstname");
+            string firstName1 = Console.ReadLine();
+            string result1 = v1.Firstname(firstName1);
+            Console.WriteLine(result1);
+
+            ValidationusingLambda v2 = new();
+            Console.WriteLine("enter Lastname");
+            string lastName1 = Console.ReadLine();
+            string result2 = v2.Lastname(lastName1);
+            Console.WriteLine(result2);
+
+            ValidationusingLambda v3 = new();
+            Console.WriteLine("enter email");
+            string email = Console.ReadLine();
+            string result3 = v3.Email(email);
+            Console.WriteLine(result3);
+
+            ValidationusingLambda v4 = new();
+            Console.WriteLine("enter mobilenumber");
+            string mobilenumber = Console.ReadLine();
+            string result4 = v4.PhoneNumber(mobilenumber);
+            Console.WriteLine(result4);
+
+            ValidationusingLambda v5 = new();
+            Console.WriteLine("enter password");
+            string password = Console.ReadLine();
+            string result5 = v5.password(password);
+            Console.WriteLine(result5);
+
+
+        }
+    }
+}
---
+++ validation/ValidationusingLambda.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+
+namespace validation
+{
+    public class ValidationusingLambda
+    {
+        public string FName_regex = "^[A-Z]{1}[a-z]{2,}$";
+        public string LName_regex = "^[A-Z]{1}[a-z]{2,}$";
+        public string MobileNumber_regex = "^[+]{1}[1-9]{2}[-. ]{1}[1-9]{2}[0-9]{8}$";
+        public string Email_regex = "^[A-Za-z](.[a-z])+@[A-Za-z]+.[a-z]{2,4}(.[a-z]{2,3})$";
+        public string Pass_regex = "^(?=.*[@#$%0-9A-Z])[@#$%0-9a-zA-Z]{8,}$";
+
+
+        public string Firstname(string firstName) => Regex.IsMatch(firstName, FName_regex) ? "First Name is Valid!" : "First Name is not Valid!";
+        public string Lastname(string lastName) => Regex.IsMatch(lastName, LName_regex) ? "Last Name is Valid!" : "Last Name is not Valid!";
+        public string Email(string email) => Regex.IsMatch(email, Email_regex) ? "Email is Valid!" : "Email is not Valid!";
+        public string PhoneNumber(string MobileNumber) => Regex.IsMatch(MobileNumber, MobileNumber_regex) ? "Phone Number is Valid!" : "Phone Number is not Valid!";
+        public string password(string password) => Regex.IsMatch(password, Pass_regex) ? "Password is Valid!" : "Password is not Valid!";
+    }
+}
+  
